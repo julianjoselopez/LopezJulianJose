4e1460505c2dd1a2398581e1ea365495518412a9
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-05 23:02:58 -0300

    20220505 es una prueba

---
+++ Clase1EjercicioInstructivo/ejercicio1.ts
@@ -0,0 +1,29 @@
+console.log(
+  "instructivo para pagar por primera vez un servicio a traves de un cajero link"
+);
+console.log(
+  "Tener a mano el numero de codigo link del servicio a pagar, ademas de saber a que rubro pertenece lo que vamos a pagar"
+);
+console.log("Ingrese la tarjeta de debito en el cajero");
+console.log("digite su clave");
+console.log("en el menu principal del cajero seleccionar la opcion PAGOS");
+console.log(
+  "una vez que hicimos eso nos va a aparecer la agenda de pagos donde figuran todos los servicios que tenemos adheridos"
+);
+console.log("seleccionar la opcion INGRESAR NUEVO IMPUESTO O SERVICIO");
+console.log(
+  "seleccionar el rubro de lo que vamos a pagar, por ejemplo LUZ, AGUA Y GAS"
+);
+console.log("en esta pantalla tenemos que ingresar el CODIGO DE PAGOSLINK");
+console.log(
+  "nos va a mostrar todas las empresas que coincidan con el formato del codigo link nuestro, seleccionar la que corresponda en nuestro caso"
+);
+console.log(
+  "una vez que seleccionamos nuestro ente nos va a aparecer en pantalla la info de la factura a pagar, corroborar que los datos coincidan con la que recibimos"
+);
+console.log(
+  "pagar y el cajero nos va a emitir un ticket con el comprobante del pago realizado"
+);
+console.log(
+  "a modo informativo es posible imprimir a futuro nuevamente ese comprobante"
+);
---
+++ Clase1EjercicioInstructivo/index.ts
@@ -0,0 +1,9 @@
+t "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>hHello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
