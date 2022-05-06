6a32f1dd94fe5b235d43318719fd0ba863afab19
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-05 23:59:32 -0300

    20220505 ejercicio area del rectangulo

---
+++ Clase2/ejercicio1.ts
@@ -0,0 +1,7 @@
+console.log(5 * 10);
+
+let base: number = 5;
+let altura: number = 10;
+let area: number = base * altura;
+
+console.log(area);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ document.getElementById("app").innerHTML = `
 <h1>Julian Lopez</h1>
 <h2>Trabajos realizados</h2>
 <div>
-  Clase 1 - Ejercicio de realizar un Instructivo
-  <a href="Clase1EjercicioInstructivo/index.html" target="_blank" rel="noopener noreferrer">click aqui</a>.
-</div>`;
+  Clase 1 - Ejercicio 1: Realizar un Instructivo
+</div>
+<div>
+  Clase 2 - Ejercicio 1: Area del Rectangulo
+</div>
+<div>
+  Clase 3 - 
+</div)
