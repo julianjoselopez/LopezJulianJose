bcaf8f0e37f17fd7e60e3bcbb8d8948d99d80b37
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-05 23:44:16 -0300

    Clase 1 Ejercicio1 final del ejercicio 1 - Instructivo para pagar una factura por cajero automatico

--- Clase1EjercicioInstructivo/index.ts
+++
@@ -1,9 +0,0 @@
-t "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>hHello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
