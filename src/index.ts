6263012d0f1f25f5783855785e13147ea1eb69d8
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-12 20:43:33 -0300

    20220512 ejercicio del formulario completo por fin me funciono, me hizo sufrif varios dias

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Julian Lopez</h1>
-<h2>Trabajos realizados</h2>
-<div>
-  Filmina 1 - Ejercicio 1: Realizar un instructivo
-</div>
-<div>
-  Filmina 2 - Ejercicio 1: Area del rectangulo
-  Filmina 2 - Ejercicio 2: Tareas de la granjera
-</div>
-<div>
-  Filmina 3 - 
-</div)`;
+  <h2>Trabajos realizados</h2>
+    <div>
+        Filmina 1 - Ejercicio 1: Realizar un instructivo
+    </div>
+    <div>
+        Filmina 2 - Ejercicio 1: Area del rectangulo
+        Filmina 2 - Ejercicio 2: Tareas de la granjera
+        Filmina 2 - Ejercicio 3: Formulario de ingreso de datos (tambien hay un sandbox de solo este ejercicio)
+        Filmina 2 - Ejercicio 4: Realizar un instructivo(almacenado en variables)
+
+    </div>
+    <div>
+        Filmina 3 - 
+    </div);
 // este es un comentario
