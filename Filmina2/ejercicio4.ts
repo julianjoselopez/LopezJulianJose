46e5e0293c2c59ecb72b8e70dbc052759eec669d
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-12 22:49:41 -0300

    Ejercicio 4 finalizado ejercicio de instructivo pero esta vez definiendo variables

---
+++ Filmina2/ejercicio4.ts
@@ -0,0 +1,49 @@
+let paso1: string;
+let paso2: string;
+let paso3: string;
+let paso4: string;
+let paso5: string;
+let paso6: string;
+let paso7: string;
+let paso8: string;
+let paso9: string;
+let paso10: string;
+let paso11: string;
+let paso12: string;
+let paso13: string;
+
+paso1 =
+  "instructivo para pagar por primera vez un servicio a traves de un cajero link";
+paso2 =
+  "Tener a mano el numero de codigo link del servicio a pagar, ademas de saber a que rubro pertenece lo que vamos a pagar";
+paso3 = "Ingrese la tarjeta de debito en el cajero";
+paso4 = "digite su clave";
+paso5 = "en el menu principal del cajero seleccionar la opcion PAGOS";
+paso6 =
+  "una vez que hicimos eso nos va a aparecer la agenda de pagos donde figuran todos los servicios que tenemos adheridos";
+paso7 = "seleccionar la opcion INGRESAR NUEVO IMPUESTO O SERVICIO";
+paso8 =
+  "seleccionar el rubro de lo que vamos a pagar, por ejemplo LUZ, AGUA Y GAS";
+paso9 = "en esta pantalla tenemos que ingresar el CODIGO DE PAGOSLINK";
+paso10 =
+  "nos va a mostrar todas las empresas que coincidan con el formato del codigo link nuestro, seleccionar la que corresponda en nuestro caso";
+paso11 =
+  "una vez que seleccionamos nuestro ente nos va a aparecer en pantalla la info de la factura a pagar, corroborar que los datos coincidan con la que recibimos";
+paso12 =
+  "pagar y el cajero nos va a emitir un ticket con el comprobante del pago realizado";
+paso13 =
+  "a modo informativo es posible imprimir a futuro nuevamente ese comprobante";
+
+console.log(paso1);
+console.log(paso2);
+console.log(paso3);
+console.log(paso4);
+console.log(paso5);
+console.log(paso6);
+console.log(paso7);
+console.log(paso8);
+console.log(paso9);
+console.log(paso10);
+console.log(paso11);
+console.log(paso12);
+console.log(paso13);
