135296fe3a6dd0afa869abe196772670fcf47e70
julianjoselopez <104107348+julianjoselopez@users.noreply.github.com>
2022-05-18 08:33:05 -0300

    Correccion en ejercicio del descuento lo corregiporque no respetaba la consigna

--- Filmina2/ejercicio8/ejercicio8.ts
+++ Filmina2/ejercicio8/ejercicio8.ts
@@ -4,6 +4,9 @@ let precioDeLista = document.forms[0].precio;
 
 btnEnv.addEventListener("click", () => {
   let precioLista: number = precioDeLista.value;
+  let descuento: number = precioLista * 0.1;
   let precioFinal: number = precioLista * 0.9;
-  console.log("el precio con descuento es ", precioFinal);
+  console.log("el precio de lista es ", precioLista);
+  console.log("el descuento es ", descuento);
+  console.log("el cliente abonará ", precioFinal);
 });
